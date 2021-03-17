2355d1db934aee51c76cc2db5d750ef38b6b0f89
Alex Novak <alexleonnovak@gmail.com>
2021-03-17 12:27:26 +0200

    added main layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainLayoutComponent} from "./shared/components/main-layout/main-layout.component";
+import {HomeComponent} from "./front/home/home.component";
+import {PostComponent} from "./front/post/post.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/',
+        pathMatch: 'full'
+      },
+      {
+        path: '',
+        component: HomeComponent
+      },
+      {
+        path: 'post/:id',
+        component: PostComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { HomeComponent } from './front/home/home.component';
+import { PostComponent } from './front/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomeComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule,
