2fecc1ecc9d5b3a48a79cf48e79968bf4e0ba9f5
Alex Novak <alexleonnovak@gmail.com>
2021-03-19 14:53:54 +0200

    added get all posts

--- src/app/admin/dashpoard-page/dashpoard-page.component.ts
+++ src/app/admin/dashpoard-page/dashpoard-page.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostService} from "../../shared/post.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashpoard-page',
   templateUrl: './dashpoard-page.component.html',
   styleUrls: ['./dashpoard-page.component.scss']
 })
-export class DashpoardPageComponent implements OnInit {
+export class DashpoardPageComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  posts: Post[] = [];
+  private postSubscription: Subscription = new Subscription();
+
+  constructor(
+    private postService: PostService
+  ) { }
 
   ngOnInit(): void {
+    this.postSubscription = this.postService.getAll().subscribe(posts => {
+      console.log(posts);
+      this.posts = posts;
+    });
   }
 
+  ngOnDestroy(): void {
+    if (this.postSubscription) {
+      this.postSubscription.unsubscribe();
+    }
+  }
+
+  remove(id: string): void {
+    console.log(id);
+  }
 }
